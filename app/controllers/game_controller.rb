00005941f44f7da94463e169fc369f58534acb00
Dima1232021 <moarobota584@gmail.com>
2021-11-21 00:11:43 +0200

    changes in the game_controller.rb index

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,8 +2,11 @@ class GameController < ApplicationController
   include CurrentUserConcern
 
   def index
-    users = User.all.map { |user| { username: user.username, id: user.id } }
+    idCurrentUser = @current_user.id
 
+    # users = User.all.map { |user| { username: user.username, id: user.id } }
+
+    users = User.all.select { |user| idCurrentUser != user.id }
     render json: users
   end
 end
